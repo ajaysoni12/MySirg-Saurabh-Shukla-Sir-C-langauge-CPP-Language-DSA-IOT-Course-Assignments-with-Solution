e3e5c6786c0ed9bcc8e62feb017d0d367154e96f
Ajay Soni <106678641+ajaysoni12@users.noreply.github.com>
2022-07-17 08:56:31 +0530

    Delete C(Operators)-Assignment-2 directory

--- C(Operators)-Assignment-2/evenOddUsingBitwise.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    num & 1 ? printf("odd") : printf("even");
-    return 0;
-}
--- C(Operators)-Assignment-2/printASCIICodeOfGivenCharacter.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    char ch; 
-    printf("Enter a character: "); 
-    scanf("%c",&ch); 
-    printf("Ascii code is: %d",ch); 
-    return 0;
-}
