691cde1fa4b41e5ba22dcecefea80a75ceb95e2f
Ajay Soni <106678641+ajaysoni12@users.noreply.github.com>
2022-08-23 08:31:44 +0530

    Add files via upload

---
+++ C++ Language Assignments/Basic of C++ - Assignment - 23/averageOfThreeNum.cpp
@@ -0,0 +1,11 @@
+/*
+6. Write a C++ program to calculate an average of 3 numbers.
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1 = 5, num2 = 4, num3 = 8;
+    cout << "Average of three numbers is: " << (num1 + num2 + num3) / 3.0f;
+    return 0;
+}
